4f644dc3c7b0b94c1fa8171d389d7aa69e61a8f9
tega <alessiotegoni4@gmail.com>
2025-01-06 15:51:40 +0100

    fix: invoice payment bug

--- src/app/invoices/[id]/payment/page.tsx
+++ src/app/invoices/[id]/payment/page.tsx
@@ -16,56 +16,60 @@ type Props = {
 };
 
 export default async function InvoicePayment({ params, searchParams }: Props) {
-  const id = parseInt((await params).id!);
-  const { session_id, status } = await searchParams;
+  const [paramsData, { status, session_id }] = await Promise.all([
+    params,
+    searchParams,
+  ]);
+
+  const id = parseInt(paramsData.id || "d");
 
   if (isNaN(id)) throw new Error("Invalid id");
 
-  const handlePayInvoice = async () => {
-    if (status !== "success" || !session_id) return "";
+  const invoice = await getInvoice(id);
+  if (!invoice) notFound();
 
+  let updatedInvoice = { ...invoice };
+
+  if (status === "success" && session_id && invoice.status === "open") {
     const { payment_status } =
       await stripe.checkout.sessions.retrieve(session_id);
-    if (payment_status === "paid") return "";
 
-    return await payInvoice(id);
-  };
-
-  const [invoice, paymentStatus] = await Promise.all([
-    getInvoice(id),
-    handlePayInvoice(),
-  ]);
-  if (!invoice) notFound();
-  if (paymentStatus === "paid") invoice.status = "paid";
+    if (payment_status === "paid") {
+      const paymentStatus = await payInvoice(id);
+      if (paymentStatus === "paid") updatedInvoice.status = "paid";
+    }
+  }
 
   return (
     <main>
       <Container>
-        {status === "canceled" ||
-          (status === "success" && !session_id && (
-            <p
-              className="bg-red-100 text-sm text-red-800 text-center p-3
+        {(status === "canceled" || (status === "success" && !session_id)) && (
+          <p
+            className="bg-red-100 text-sm text-red-800 text-center p-3
               rounded-lg mb-5 border border-red-200 dark:bg-red-900 dark:text-red-200 dark:border-red-800"
-            >
-              Something went wrong, please try again
-            </p>
-          ))}
+          >
+            Something went wrong, please try again
+          </p>
+        )}
         <div className="grid grid-cols-2">
           <div className="space-y-4">
             <header className="flex justify-between items-center mb-6">
               <div className="flex items-center gap-4">
                 <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-                  Invoice #{invoice.id}
+                  Invoice #{updatedInvoice.id}
                 </h1>
-                <StatusBadge status={invoice.status} className="self-start" />
+                <StatusBadge
+                  status={updatedInvoice.status}
+                  className="self-start"
+                />
               </div>
             </header>
 
             <div className="text-2xl font-semibold text-gray-600 dark:text-gray-300">
-              ${(invoice.value / 100).toFixed(2)}
+              ${(updatedInvoice.value / 100).toFixed(2)}
             </div>
             <p className="text-gray-600 dark:text-gray-400">
-              {invoice.description}
+              {updatedInvoice.description}
             </p>
 
             <section>
@@ -78,7 +82,7 @@ export default async function InvoicePayment({ params, searchParams }: Props) {
                     Invoice ID
                   </span>
                   <span className="text-lg text-gray-700 dark:text-gray-300">
-                    {invoice.id}
+                    {updatedInvoice.id}
                   </span>
                 </div>
                 <div className="grid grid-cols-2">
@@ -86,7 +90,7 @@ export default async function InvoicePayment({ params, searchParams }: Props) {
                     Invoice Date
                   </span>
                   <span className="text-lg text-gray-700 dark:text-gray-300">
-                    {new Date(invoice.createdAt).toLocaleDateString()}
+                    {new Date(updatedInvoice.createdAt).toLocaleDateString()}
                   </span>
                 </div>
                 <div className="grid grid-cols-2">
@@ -94,7 +98,7 @@ export default async function InvoicePayment({ params, searchParams }: Props) {
                     Billing Name
                   </span>
                   <span className="text-lg text-gray-700 dark:text-gray-300">
-                    {invoice.customer.name}
+                    {updatedInvoice.customer.name}
                   </span>
                 </div>
               </div>
@@ -104,16 +108,16 @@ export default async function InvoicePayment({ params, searchParams }: Props) {
             <h2 className="text-2xl font-bold mb-4 text-gray-800 dark:text-gray-200">
               Manage Invoice
             </h2>
-            {invoice.status === "open" && (
+            {updatedInvoice.status === "open" && (
               <form action={createPayment}>
-                <input type="hidden" name="id" value={invoice.id} />
+                <input type="hidden" name="id" value={updatedInvoice.id} />
                 <Button className="bg-blue-500 text-white dark:bg-blue-600">
                   <CreditCard className="w-5 h-auto" />
                   Pay Invoice
                 </Button>
               </form>
             )}
-            {invoice.status === "paid" && (
+            {updatedInvoice.status === "paid" && (
               <Button
                 className="bg-green-600 text-white !opacity-80 dark:bg-green-700"
                 disabled
